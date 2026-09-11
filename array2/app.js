63440b8c86da8383d810383ba0a658cd55b1533f
khadijacode123-hue <khadija.code123@gmail.com>
2026-09-11 17:03:51 +0500

    arrrays

---
+++ array2/app.js
@@ -0,0 +1,16 @@
+// ===>Q   u   e   s   t   i   o   n     1 <====
+var arr = []
+console.log(arr);
+// ===>Q   u   e   s   t   i   o   n     2 <====
+var arr2 = new Array()
+console.log(arr2);
+
+// ===>Q   u   e   s   t   i   o   n     3 <====
+var stringArr = ["Apple", "Banana ", "Mango"]
+console.log(stringArr);
+// ===>Q   u   e   s   t   i   o   n     4 <====
+var numberArr = [1, 2, 3, 4, 5, 6]
+console.log(numberArr);
+// ===>Q   u   e   s   t   i   o   n     5 <====
+var booleanArr = [true, false]
+console.log(booleanArr);
